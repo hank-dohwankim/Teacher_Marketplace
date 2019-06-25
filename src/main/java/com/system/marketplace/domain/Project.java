d451575b87d8975abed436046d57f2681e56467c
hank.dohwankim <hank.dohwankim@gmail.com>
2019-06-25 00:22:10 -0500

    User update

--- src/main/java/com/system/marketplace/domain/Project.java
+++ src/main/java/com/system/marketplace/domain/Project.java
@@ -1,126 +1,99 @@
 package com.system.marketplace.domain;
 
-
-import lombok.Data;
-
-@Data
 public class Project {
-
-    private int project_id;
-    private int proposal_id;
-    private int user_id;
-    private String theme_id;
-    private String project_subject;
-    private String project_requirement;
-    private String project_dec;
-    private String project_start;
-    private String project_end;
-    private float wage_hour;
-    private float wage_day;
-    private float amount_offer;
+    private int theme_id;
+    private String subject;
+    private String subject_detail;
+    private String category;
+    private String internal_order_id;
+    private String lecture_period;
+    private String lecture_wage;
+    private String lecture_wage_range;
+    private String address;
 
     public Project() {}
 
-    public Project(int project_id, int proposal_id, int user_id, String theme_id, String project_subject, String project_dec, String project_start, String project_end, float wage_hour, float wage_day, float amount_offer) {
-        this.project_id = project_id;
-        this.proposal_id = proposal_id;
-        this.user_id = user_id;
+    public Project(int theme_id, String subject, String subject_detail, String category, String internal_order_id, String lecture_period, String lecture_wage, String lecture_wage_range, String address) {
         this.theme_id = theme_id;
-        this.project_subject = project_subject;
-        this.project_dec = project_dec;
-        this.project_start = project_start;
-        this.project_end = project_end;
-        this.wage_hour = wage_hour;
-        this.wage_day = wage_day;
-        this.amount_offer = amount_offer;
+        this.subject = subject;
+        this.subject_detail = subject_detail;
+        this.category = category;
+        this.internal_order_id = internal_order_id;
+        this.lecture_period = lecture_period;
+        this.lecture_wage = lecture_wage;
+        this.lecture_wage_range = lecture_wage_range;
+        this.address = address;
     }
 
-    public int getProject_id() {
-        return project_id;
-    }
-
-    public void setProject_id(int project_id) {
-        this.project_id = project_id;
-    }
-
-    public int getProject_detail_id() {
-        return proposal_id;
-    }
-
-    public void setProject_detail_id(int proposal_id) {
-        this.proposal_id = proposal_id;
-    }
-
-    public int getUser_id() {
-        return user_id;
-    }
-
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getTheme_id() {
+    public int getTheme_id() {
         return theme_id;
     }
 
-    public void setTheme_id(String theme_id) {
+    public void setTheme_id(int theme_id) {
         this.theme_id = theme_id;
     }
 
-    public String getProject_name() {
-        return project_subject;
+    public String getSubject() {
+        return subject;
     }
 
-    public void setProject_name(String project_subject) {
-        this.project_subject = project_subject;
+    public void setSubject(String subject) {
+        this.subject = subject;
     }
 
-    public String getProject_dec() {
-        return project_dec;
+    public String getSubject_detail() {
+        return subject_detail;
     }
 
-    public void setProject_dec(String project_dec) {
-        this.project_dec = project_dec;
+    public void setSubject_detail(String subject_detail) {
+        this.subject_detail = subject_detail;
     }
 
-    public String getProject_start() {
-        return project_start;
+    public String getCategory() {
+        return category;
     }
 
-    public void setProject_start(String project_start) {
-        this.project_start = project_start;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public String getProject_end() {
-        return project_end;
+    public String getInternel_order_id() {
+        return internal_order_id;
     }
 
-    public void setProject_end(String project_end) {
-        this.project_end = project_end;
+    public void setInternel_order_id(String internal_order_id) {
+        this.internal_order_id = internal_order_id;
     }
 
-    public float getWage_hour() {
-        return wage_hour;
+    public String getLecture_period() {
+        return lecture_period;
     }
 
-    public void setWage_hour(float wage_hour) {
-        this.wage_hour = wage_hour;
+    public void setLecture_period(String lecture_period) {
+        this.lecture_period = lecture_period;
     }
 
-    public float getWage_day() {
-        return wage_day;
+    public String getLecture_wage() {
+        return lecture_wage;
     }
 
-    public void setWage_day(float wage_day) {
-        this.wage_day = wage_day;
+    public void setLecture_wage(String lecture_wage) {
+        this.lecture_wage = lecture_wage;
     }
 
-    public float getAmount_offer() {
-        return amount_offer;
+    public String getLecture_wage_range() {
+        return lecture_wage_range;
     }
 
-    public void setAmount_offer(float amount_offer) {
-        this.amount_offer = amount_offer;
+    public void setLecture_wage_range(String lecture_wage_range) {
+        this.lecture_wage_range = lecture_wage_range;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 }
-
--- src/main/java/com/system/marketplace/domain/User.java
+++ src/main/java/com/system/marketplace/domain/User.java
@@ -1,197 +1,64 @@
 package com.system.marketplace.domain;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.util.List;
+import javax.persistence.*;
 
-@Data
+@Entity
+@Table(name="tbl_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
     private int user_id;
-    private String user_photo;
-    private String user_name;
-    private String user_login;
+
+    @Column
+    private String user_email;
+
+    @Column
     private String user_password;
-    private String user_type;
-    private String user_firstName;
-    private String user_LastName;
-    private String user_lastName;
+
+    @Column
+    private UserType type;
+
+    @Column
+    private String teacher_firstName;
+
+    @Column
+    private String teacher_lastName;
+
+    @Column
     private String telephone_home;
+
+    @Column
     private String telephone_work;
-    private String user_address;
-    private String user_mail;
-    private String currentLocation;
-    private String nationality;
-    private String website;
 
-    public User() {}
+    @Column
+    private String address_country;
 
-    public User(int user_id, String user_photo, String user_name, String user_login, String user_password, String user_type, String user_firstName, String user_LastName, String user_lastName, String telephone_home, String telephone_work, String user_address, String user_mail, String currentLocation, String nationality, String website) {
-        this.user_id = user_id;
-        this.user_photo = user_photo;
-        this.user_name = user_name;
-        this.user_login = user_login;
-        this.user_password = user_password;
-        this.user_type = user_type;
-        this.user_firstName = user_firstName;
-        this.user_LastName = user_LastName;
-        this.user_lastName = user_lastName;
-        this.telephone_home = telephone_home;
-        this.telephone_work = telephone_work;
-        this.user_address = user_address;
-        this.user_mail = user_mail;
-        this.currentLocation = currentLocation;
-        this.nationality = nationality;
-        this.website = website;
-    }
+    @Column
+    private String address_state;
 
-    public int getUser_id() {
-        return user_id;
-    }
+    @Column
+    private String address_city;
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
+    @Column
+    private String address_street;
 
-    public String getUser_photo() {
-        return user_photo;
-    }
+    @Column
+    private String address_postal;
 
-    public void setUser_photo(String user_photo) {
-        this.user_photo = user_photo;
-    }
+    @Column
+    private String user_website;
 
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getUser_login() {
-        return user_login;
-    }
-
-    public void setUser_login(String user_login) {
-        this.user_login = user_login;
-    }
-
-    public String getUser_password() {
-        return user_password;
-    }
-
-    public void setUser_password(String user_password) {
-        this.user_password = user_password;
-    }
-
-    public String getUser_type() {
-        return user_type;
-    }
-
-    public void setUser_type(String user_type) {
-        this.user_type = user_type;
-    }
-
-    public String getUser_firstName() {
-        return user_firstName;
-    }
-
-    public void setUser_firstName(String user_firstName) {
-        this.user_firstName = user_firstName;
-    }
-
-    public String getUser_LastName() {
-        return user_LastName;
-    }
-
-    public void setUser_LastName(String user_LastName) {
-        this.user_LastName = user_LastName;
-    }
-
-    public String getUser_lastName() {
-        return user_lastName;
-    }
-
-    public void setUser_lastName(String user_lastName) {
-        this.user_lastName = user_lastName;
-    }
-
-    public String getTelephone_home() {
-        return telephone_home;
-    }
-
-    public void setTelephone_home(String telephone_home) {
-        this.telephone_home = telephone_home;
-    }
-
-    public String getTelephone_work() {
-        return telephone_work;
-    }
-
-    public void setTelephone_work(String telephone_work) {
-        this.telephone_work = telephone_work;
-    }
-
-    public String getUser_address() {
-        return user_address;
-    }
-
-    public void setUser_address(String user_address) {
-        this.user_address = user_address;
-    }
-
-    public String getUser_mail() {
-        return user_mail;
-    }
-
-    public void setUser_mail(String user_mail) {
-        this.user_mail = user_mail;
-    }
-
-    public String getCurrentLocation() {
-        return currentLocation;
-    }
-
-    public void setCurrentLocation(String currentLocation) {
-        this.currentLocation = currentLocation;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public String getWebsite() {
-        return website;
-    }
-
-    public void setWebsite(String website) {
-        this.website = website;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "user_id=" + user_id +
-                ", user_photo='" + user_photo + '\'' +
-                ", user_name='" + user_name + '\'' +
-                ", user_login='" + user_login + '\'' +
-                ", user_password='" + user_password + '\'' +
-                ", user_type='" + user_type + '\'' +
-                ", user_firstName='" + user_firstName + '\'' +
-                ", user_LastName='" + user_LastName + '\'' +
-                ", user_lastName='" + user_lastName + '\'' +
-                ", telephone_home='" + telephone_home + '\'' +
-                ", telephone_work='" + telephone_work + '\'' +
-                ", user_address='" + user_address + '\'' +
-                ", user_mail='" + user_mail + '\'' +
-                ", currentLocation='" + currentLocation + '\'' +
-                ", nationality='" + nationality + '\'' +
-                ", website='" + website + '\'' +
-                '}';
-    }
 }
